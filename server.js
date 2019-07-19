732a0f065dcf558289718557c7309b22f5e03547
1138433234 <1138433234@qq.com>
2019-07-19 10:48:20 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+﻿var ws = require('nodejs-websocket');
+var moment=require('moment');
+
+
+var server = ws.createServer(function (conn) {
+    console.log('new connection');
+    conn.on('text',function(str){
+   
+   
+    
+    var time=moment(new Date()).format('YYYY-MM-DD hh:mm:ss');
+   
+    
+    boardcast(time+':'+str);
+    });
+    conn.on('err',function(err){
+       if(err){
+           console.log('connection false');
+       }
+    });
+   
+    
+
+})
+server.listen(8088);
+
+function boardcast(str){
+    server.connections.forEach(function(conn){
+        conn.sendText(str);
+    });
+}
